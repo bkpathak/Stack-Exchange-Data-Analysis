f66a3d1bba4f70b14b6c83d151651821fddf3cb8
bkpathak <bkpathak@mtu.edu>
2015-03-25 17:54:47 -0700

    added code to find top 10 user, unique badge and 10 oldest user

--- src/main/scala/com/cloudwick/spark/StackExchangeAnalysis.scala
+++ src/main/scala/com/cloudwick/spark/StackExchangeAnalysis.scala
@@ -13,7 +13,7 @@ case class Users(reputation: Int, displayName: String, age: Int)
 object Users {
 
   //Regex to find reputaion and display name
-  val Regex = """Reputation="(\d+)".+DisplayName="([A-Za-z0-9_ ]*)".+Age="([0-9]{2})"""".r
+  val Regex = """^.+Reputation="(\d+)".+DisplayName="([A-Za-z0-9_ ]*)".+Age="([0-9]{2})" .*$""".r
 
   def getDisplayNameReputation(row: String) = row match {
     case Regex(reputation, displayName, age) => Some(Users(reputation.toInt, displayName, age.toInt))
@@ -54,7 +54,7 @@ object StackExchangeAnalysis {
     val sparkConf = new SparkConf()
     //set conf to use Kryo Serializer
 
-    sparkConf.set("spark.kryo.registrator", "sparking.MyKryoRegistrator")
+    sparkConf.set("spark.kryo.registrator", "com.cloudwick.spark.MyKryoRegistrator")
     sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     sparkConf.set("spark.rdd.compress", "true")
 
@@ -67,14 +67,32 @@ object StackExchangeAnalysis {
     val sc = new SparkContext(sparkConf)
 
     //read the input file and number of partitions, the second argument is number of partition to create
-    val users = sc.textFile(args(0), args(2).toInt)
-    val badges = sc.textFile(args(1), args(2).toInt)
+    val users = sc.textFile(args(0))
+    val badges = sc.textFile(args(1))
+
+    //  parse the base users RDD and the result is cache
+    val usersInfo = users.flatMap(line => Users.getDisplayNameReputation(line.toString)).cache()
 
     //  top users based on their reputation
-    val topUsers = users.flatMap(Users.getDisplayNameReputation)
-      .map(data => data.reputation -> data.displayName)
-      .sortByKey()
+    val topUsers = usersInfo.map(data => data.reputation -> data.displayName)
+      .sortByKey(ascending = false)
+
     println("Top 10 users according to reputation")
     topUsers.take(10).foreach(println)
+
+    //all unique badges present
+    val uniqueBadges = badges.flatMap(line => Badge.getIdBadge(line))
+      .map(data => data.badgeName)
+      .distinct()
+
+    println("All the unique badge name present")
+    uniqueBadges.collect().foreach(println)
+
+    // 10 most older users in the group
+    val olderUser = usersInfo.map(data => data.age -> data.displayName)
+      .sortByKey(ascending = false)
+
+    println("10 oldest users")
+    olderUser.take(10).foreach(println)
   }
 }
